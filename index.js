c4094ed0438bf6d5c79d2eb14b9d72c820ad43ec
kaziabulhasib <hasibkazi420@gmail.com>
2024-08-23 11:14:01 +0530

    contact second route

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.send("<h1>Contact me for details .............</h1>");
 });
+app.get("/contactme", (req, res) => {
+  res.send("<h1>Contact me for details 222.............</h1>");
+});
 
 app.listen(port, () => {
   console.log(`First express app listening on port ${port}`);
